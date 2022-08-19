b40c5e7cc224e187d248ffa643a2147c128f3449
Axel Lara <m.axellaraa@gmail.com>
2022-08-19 14:31:05 -0500

    A: styles, html5 structure - F -

--- routes/routing.php
+++ routes/routing.php
@@ -7,9 +7,11 @@ $route->addRoute("/", fn() => "<h1>Home Page</h1>");
 
 $route->addRoute("/home", fn() => "<h1>Home Page</h1>");
 
-$route->addRoute("/login", fn() => "<h1>Login Page</h1>");
+$route->addRoute("/contact", fn() => "<h1>Contact Page</h1>");
 
-$route->addRoute("/register", fn() => "<h1>Register Page</h1>");
+$route->addRoute("/services", fn() => "<h1>Services Page</h1>");
+
+$route->addRoute("/login", fn() => "<h1>Login Page</h1>");
 
 $route->routing();
 
